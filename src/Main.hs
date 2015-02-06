b1fe8dbd7696f4ce3eb3e2aa7a373f8d06c83cea
fpuser2579653 <yom@artyom.me>
2015-02-06 12:30:56 -0800

    Move types/complexities around.

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import           Control.Applicative
 import           Control.Monad
-import           Data.List (intercalate)
+import           Data.List (intercalate, nub)
 import           Data.Maybe
 import           Data.Yaml
 import qualified System.FilePath.Find as Find
@@ -16,27 +16,31 @@ data Entry = Entry
     functionName            :: String
   , functionPackage         :: String
   , functionModules         :: [String]
+  , functionComplexity      :: Maybe String
   , functionImplementations :: [Implementation]
   }
 
 data Implementation = Implementation
   {
     implementationName       :: String
+  , implementationType       :: String
   , implementationComplexity :: Maybe String
   , implementationCode       :: String
   }
 
 instance FromJSON Entry where
   parseJSON (Object v) = Entry
-    <$> v .: "name"
-    <*> v .: "package"
-    <*> v .: "modules"
-    <*> v .: "implementations"
+    <$> v .:  "name"
+    <*> v .:  "package"
+    <*> v .:  "modules"
+    <*> v .:? "complexity"
+    <*> v .:  "implementations"
   parseJSON _          = mzero
 
 instance FromJSON Implementation where
   parseJSON (Object v) = Implementation
     <$> v .:  "name"
+    <*> v .:  "type"
     <*> v .:? "complexity"
     <*> v .:  "code"
   parseJSON _          = mzero
@@ -46,20 +50,32 @@ indent n = unlines . map (replicate n ' ' ++) . lines
 
 showEntry :: Entry -> String
 showEntry e =
-  printf "function: %s\n" (functionName e) ++
-  printf "available from: %s (%s)\n"
+  -- Name.
+  functionName e ++ "\n" ++
+  -- Signatures.
+  concatMap (printf "  :: %s\n")
+            (nub . map implementationType . functionImplementations $ e) ++
+  "\n" ++
+  -- Package, modules.
+  printf "available from %s (%s)\n"
     (functionPackage e)
     (intercalate ", " $ functionModules e) ++
-  "implementations:\n\n" ++
-  intercalate "\n" (map (indent 2 . showImplementation) 
+  "\n" ++
+  -- Implementations.
+  "implementations:\n" ++
+  "\n" ++
+  intercalate "\n" (map (indent 2 . showImplementation (functionName e))
                         (functionImplementations e))
 
-showImplementation :: Implementation -> String
-showImplementation e =
+showImplementation :: String -> Implementation -> String
+showImplementation name e =
   printf "%s:\n" (implementationName e) ++
-  maybe "" (printf "  complexity: %s\n") (implementationComplexity e) ++
-  "  code:\n" ++
-  indent 4 (implementationCode e)
+  indent 4 code
+  where
+    code = maybe "" (printf "-- complexity: %s\n")
+                    (implementationComplexity e) ++
+           printf "%s :: %s\n" name (implementationType e) ++
+           implementationCode e
 
 main :: IO ()
 main = do
